8a234ff157c90c4346181fe9a7c82f84aed6206c
JudahSilva <jusilva@csumb.edu>
2023-05-11 18:10:16 -0700

    Use case 05; allow logging in

--- app/src/main/java/com/example/getsy_v2/LoginActivity.java
+++ app/src/main/java/com/example/getsy_v2/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.example.getsy_v2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
 import android.content.Context;
 import android.content.Intent;
@@ -8,41 +9,79 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.getsy_v2.DB.AppDatabase;
+import com.example.getsy_v2.DB.UserDAO;
 import com.example.getsy_v2.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
 
-    ActivityLoginBinding binding;
+    private ActivityLoginBinding binding;
 
-    EditText mUsername;
-    EditText mPassword;
+    private EditText mUsernameInput;
+    private EditText mPasswordInput;
 
-    Button mLogIn;
+    private String mUsername;
+    private String mPassword;
+
+    private Button mLogIn;
+
+    private UserDAO UserDAO;
+
+    private User mUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        getDatabase();
+
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        mUsername = binding.childUserNameInput;
-        mPassword = binding.childPasswordInput;
+        mUsernameInput = binding.childUserNameInput;
+        mPasswordInput = binding.childPasswordInput;
         mLogIn = binding.childLogInButton;
 
         mLogIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                checkCredentials();
-                backToMain();
+                getDataFromDisplay();
+                if (!checkCredentials()) {
+                    backToMain();
+                }
+                forwardUntoLanding();
             }
         });
     }
 
-    private void checkCredentials() {
+    private void forwardUntoLanding() {
+        Intent intent = LandingActivity.intentFactory(this);
+        startActivity(intent);
+    }
 
+    private void getDataFromDisplay() {
+        mUsername = mUsernameInput.getText().toString();
+        mPassword = mPasswordInput.getText().toString();
+    }
+
+    private boolean checkCredentials() {
+        mUser = UserDAO.getUserByUsername(mUsername);
+        if (mUser == null) {
+            Toast.makeText(this, "Incorrect username or password; returning to main activity", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if (!mUser.getPassword().equals(mPassword)) {
+            Toast.makeText(this, "Incorrect username or password; returning to main activity", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
+    }
+
+    private void getDatabase() {
+        UserDAO = Room.databaseBuilder(this, AppDatabase.class, AppDatabase.DATABASE_NAME).build().UserDAO();
     }
 
     private void backToMain() {

107671b22df05229bc5e7ab9bed27519505ab457
JudahSilva <jusilva@csumb.edu>
2023-05-12 22:09:41 -0700

    create new tables and update dao

--- app/src/main/java/com/example/getsy_v2/DB/AppDatabase.java
+++ app/src/main/java/com/example/getsy_v2/DB/AppDatabase.java
@@ -6,12 +6,16 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
+import com.example.getsy_v2.Product;
+import com.example.getsy_v2.ShoppingCart;
 import com.example.getsy_v2.User;
 
-@Database(entities = {User.class}, version = 1)
+@Database(entities = {User.class, ShoppingCart.class, Product.class}, version = 2)
 public abstract class AppDatabase extends RoomDatabase {
     public static final String DATABASE_NAME = "Getsy.db";
     public static final String USER_TABLE = "user_table";
+    public static final String SHOPPING_CART_TABLE = "shopping_cart_table";
+    public static final String PRODUCT_TABLE = "product_table";
 
     private static volatile AppDatabase instance;
     public static final Object LOCK = new Object();
--- app/src/main/java/com/example/getsy_v2/DB/UserDAO.java
+++ app/src/main/java/com/example/getsy_v2/DB/UserDAO.java
@@ -6,6 +6,8 @@ import androidx.room.Insert;
 import androidx.room.Query;
 import androidx.room.Update;
 
+import com.example.getsy_v2.Product;
+import com.example.getsy_v2.ShoppingCart;
 import com.example.getsy_v2.User;
 
 import java.util.List;
@@ -30,5 +32,38 @@ public interface UserDAO {
 
     @Query(("SELECT * FROM " + AppDatabase.USER_TABLE + " WHERE mUsername = :userName"))
     User getUserByUsername(String userName);
+
+    @Insert
+    void insert(ShoppingCart...carts);
+
+    @Update
+    void update(ShoppingCart...carts);
+
+    @Delete
+    void delete(ShoppingCart cart);
+
+    @Query(" SELECT * FROM " + AppDatabase.SHOPPING_CART_TABLE)
+    List<ShoppingCart> getAllCarts();
+
+    @Query(" SELECT * FROM " + AppDatabase.SHOPPING_CART_TABLE + " WHERE mCartId = :cartId")
+    ShoppingCart getCartByCartId(int cartId);
+
+    @Query(" SELECT * FROM " + AppDatabase.SHOPPING_CART_TABLE + " WHERE mUserId = :userId")
+    ShoppingCart getCartByUserId(int userId);
+
+    @Insert
+    void insert(Product...products);
+
+    @Update
+    void update(Product...products);
+
+    @Delete
+    void delete(Product product);
+
+    @Query(" SELECT * FROM " + AppDatabase.PRODUCT_TABLE)
+    List<Product> getAllProducts();
+
+    @Query(" SELECT * FROM " + AppDatabase.PRODUCT_TABLE + " WHERE mProductId = :productId")
+    Product getProductById(int productId);
 
 }
